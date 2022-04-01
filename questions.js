c4eab0ac67b409ee95c9a64800d1ae0bb9c05b35
NkanorJ <71755372+NkanorJ@users.noreply.github.com>
2022-04-01 18:49:32 +0100

    Create questions.js

---
+++ questions.js
@@ -0,0 +1,71 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+  {
+  numb: 1,
+  question: "What is the capital of India? ",
+  answer: "New Delhi",
+  options: [
+    "New Delhi",
+   	"Bengaluru",
+    "Caracas",
+    "Bueno Aires"
+  ]
+},
+  {
+  numb: 2,
+  question: "What year did Nigeria gain independence?",
+  answer: "1960",
+  options: [
+    "1966",
+    "1906",
+    "1960",
+    "1963"
+  ]
+},
+  {
+  numb: 3,
+  question: "Which of these is a continent?",
+  answer: "Australia",
+  options: [
+    "United State of America",
+    "Australia",
+    "Great Britian",
+    "Russia"
+  ]
+},
+  {
+  numb: 4,
+  question: "4.	What is the largest internal organ in the human body?",
+  answer: "Liver",
+  options: [
+    "Stomach",
+    "Heart",
+    "Liver",
+    "Large Intestine"
+  ]
+},
+  {
+  numb: 5,
+  question: "Which of these elements is not part of the first twenty elements in the periodic table?",
+  answer: "Zinc",
+  options: [
+    "Zinc",
+    "Oxygen",
+    "Nitrogen",
+    "Carbon"
+  ]
+},
+// you can uncomment the below codes and make duplicate as more as you want to add question
+// but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+//   {
+//   numb: 6,
+//   question: "Your Question is Here",
+//   answer: "Correct answer of the question is here",
+//   options: [
+//     "Option 1",
+//     "option 2",
+//     "option 3",
+//     "option 4"
+//   ]
+// },
+];
